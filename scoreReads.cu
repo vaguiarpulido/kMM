2a345969eaa7310afda03759f65b32a0e4fca9e3
tcickovs <tcickovs@fiu.edu>
2015-10-06 13:32:34 -0400

    Fixed to now just use the whole genome, since N's are in the k-Mers.

--- scoreReads.cu
+++ scoreReads.cu
@@ -1,34 +1,8 @@
 // TMC Faster
-__constant__ int mapping[20] = {0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-
-__device__ int mapKmer(char * str, int loc, int len) {
-  // Takes a substring of str starting from loc of length len and computes its mapped value
-  int mapVal = 0;
-  for(int i = loc; i < len; i++) {
-    mapVal = 4 * mapVal + mapping[(int)str[i]-65];
-  }
-  return mapVal;
-}
-
-/*__global__ void removeNs(char* genome, int seqLength, char** sequences, int* lengths) {
-   int i = threadIdx.x;
-
-   int startspot = i*seqLength;
-   int endspot = startspot + seqLength - 1;
- 
-   int j;
-   int count = 0;
-   for (j = startspot; j <= endspot; j++) {
-      if (genome[j] != 'N') {
-         sequences[i][count] = genome[j];
-         count++;
-      }
-   }
-   lengths[i] = count;
-}*/
+__constant__ int mapping[20] = {0, -1, 3, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1};
 
 // Thread i will score genome[i*seqlength] to genome[i*seqlength+(seqlength-1)]
-__global__ void scoreReads(char** sequences, int* lengths, int order, float* model, float* scores) {
+__global__ void scoreReads(char* genome, int seqLength, int order, float* model, float* scores) {
    int i = blockIdx.x;  // Thread identifier, assign to i
    int j = threadIdx.x;
    
@@ -37,26 +11,30 @@ __global__ void scoreReads(char** sequences, int* lengths, int order, float* mod
    extern __shared__ float kmer_scores[];  // Call this with [lengths[i] / order + 1];
 
    // Start spot
-   int startspot = j*order;
+   int seqspot = i*seqLength;
+   int startspot = seqspot+(j*order);
 
    // Quick loop, check for n's
+   // Actually, decided to inline it rather than loop twice.
    int a;
-   bool flag = false;
+   bool nFlag = false;
+   int mapVal = 0;
    for (a = startspot; a < startspot+order; a++) {
-      if (sequences[i][a] == 'N') { 
-         flag = true;
+      if (genome[a] == 'N') { 
+         kmer_scores[j] = 0;
+         nFlag = true;
          break;
       }
+      else 
+         mapVal = 4*mapVal + mapping[(int)genome[a]-65];
    }
 
-   if (flag)
-      kmer_scores[j] = 0;
-   else
-      kmer_scores[j] = model[mapKmer(sequences[i], j*order, order)];
+   if (!nFlag)
+      kmer_scores[j] = model[mapVal];
 
    __syncthreads();
    // Do the addition in parallel as well.
-   int k = lengths[i] / 2;
+   int k = seqLength / 2;
    while (k >= 1) {
       kmer_scores[j] = kmer_scores[j] + kmer_scores[j+k];
       k /= 2;
